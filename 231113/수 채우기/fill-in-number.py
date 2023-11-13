d5c9c1186ee19684b449e2f4109f38170b22f704
Wanhui_Lee <100890687+leewanhui-202102581@users.noreply.github.com>
2023-11-13 22:25:45 +0900

    Title: 수 채우기, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 231113/수 채우기/fill-in-number.py
+++ 231113/수 채우기/fill-in-number.py
@@ -1,12 +1,14 @@
 n = int(input())
 
-
+cnt = 0
 
 cnt = n // 5
 n = n % 5
 
+if cnt == 0 and n%2== 1:
+    print(-1)
 #나머지가 1일 때, 3일 때,
-if n == 1 or n == 4:
+elif n == 1 or n == 4:
     print(cnt + 2) # -1 + 3
 elif n == 3:
     print(cnt + 3)
