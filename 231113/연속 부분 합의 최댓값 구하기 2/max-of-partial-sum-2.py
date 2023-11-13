8addf01b2cde76571e4ffd09e141c91c1755914c
Wanhui_Lee <100890687+leewanhui-202102581@users.noreply.github.com>
2023-11-13 20:17:18 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 130ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231113/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.py
@@ -0,0 +1,14 @@
+n = int(input())
+arr = [int(a) for a in input().split()]
+
+arr_sum = 0
+result = 0
+for i in range(n):
+    if arr[i] + arr_sum >= 0:
+        arr_sum += arr[i]
+    else: 
+        arr_sum = 0
+    
+    result = max(result, arr_sum)
+
+print(result)
