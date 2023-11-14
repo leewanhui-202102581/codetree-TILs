62e9b5e9b4fbe3a0ca75ba5ac54e960a40d8f98f
Wanhui_Lee <100890687+leewanhui-202102581@users.noreply.github.com>
2023-11-14 09:26:10 +0900

    Title: 회의실 겹치지 않게 하기, Time: 527ms, Memory: 30MB, Status: Passed - CodeTree

---
+++ 231114/회의실 겹치지 않게 하기/do-not-overlap-the-meeting-room.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+arr = []
+for _ in range(n):
+    s, e = input().split()
+    arr.append((int(e), int(s)))
+    
+arr.sort()  #끝시간이 빠른 순으로 정렬
+
+curr_time = 0
+result = 0
+for (e, s) in arr:
+    if s >= curr_time:
+        result += 1
+        curr_time = e
+
+print(n-result)
