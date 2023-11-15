66d8986c5899ab30ab3a092ff852f4216f881345
Wanhui_Lee <100890687+leewanhui-202102581@users.noreply.github.com>
2023-11-15 18:39:20 +0900

    Title: 폭탄 해체 작업, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231115/폭탄 해체 작업/the-bomb-dismantling.py
+++ 231115/폭탄 해체 작업/the-bomb-dismantling.py
@@ -13,7 +13,7 @@ i = 0 #index
 while i < n:
 
     while i < n and arr[i][1] >= curr_time: 
-        heappush(candidate, (-arr[i][0], -arr[i][1]) ) #기준1: 큰 점수 기준2: 큰 시간
+        heappush(candidate, (-arr[i][0], arr[i][1]) ) #기준1: 큰 점수 기준2: 큰 시간
         i += 1
     
     if candidate :
@@ -23,6 +23,6 @@ while i < n:
         result += score
         curr_time -= 1
     else : 
-        curr_time = arr[i][1]
+        curr_time -= 1
     
 print(result)
