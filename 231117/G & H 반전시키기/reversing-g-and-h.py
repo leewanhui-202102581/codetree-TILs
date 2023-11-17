df5701818fd743268266fa8b74c23302d437d1ff
Wanhui_Lee <100890687+leewanhui-202102581@users.noreply.github.com>
2023-11-17 17:14:03 +0900

    Title: G & H 반전시키기, Time: 103ms, Memory: 24MB, Status: Passed - CodeTree

--- 231117/G & H 반전시키기/reversing-g-and-h.py
+++ 231117/G & H 반전시키기/reversing-g-and-h.py
@@ -7,13 +7,13 @@ result = 0
 changing = False
 
 for i in range(n):
-    if changing:
-        if string[i] != target[i]:
+    if changing: #바꾸는 중이라면. 
+        if string[i] == target[i]:
             changing = False
 
         # 두 문자가 같을 때는 그냥 pass
-    else:
-        if string[i] == target[i]:
+    else:#바꾸지 않는 중이라면. 
+        if string[i] != target[i]:
             changing = True
             result += 1
 
