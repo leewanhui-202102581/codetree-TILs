15100895e30255c855b401fc6c0815707d6ed65d
Wanhui_Lee <100890687+leewanhui-202102581@users.noreply.github.com>
2023-11-17 17:28:46 +0900

    Title: 좌우 반전시키기, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231117/좌우 반전시키기/flip-left-and-right.py
@@ -0,0 +1,16 @@
+n = int(input())
+string = list(map(int, input().split()))
+cnt =0
+
+
+for i in range(1, n-1): #마지막 n번째 자리는 따로 처리해주기. 
+    if string[i-1] == 0:
+        string[i-1] = abs(string[i-1] -1)
+        string[i] =   abs(string[i]  - 1)
+        string[i+1] = abs(string[i+1] - 1)
+        cnt += 1
+
+if string[n-2] != string[n-1]:
+    print(-1)
+else:
+    print(cnt + abs(string[n-1] -1))
