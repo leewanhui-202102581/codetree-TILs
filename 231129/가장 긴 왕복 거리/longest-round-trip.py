f663560a3f92ccd4e65b24cf60b8e4569953347b
Wanhui_Lee <100890687+leewanhui-202102581@users.noreply.github.com>
2023-11-29 17:39:22 +0900

    Title: 가장 긴 왕복 거리, Time: 213ms, Memory: 38MB, Status: Passed - CodeTree

--- 231129/가장 긴 왕복 거리/longest-round-trip.py
+++ 231129/가장 긴 왕복 거리/longest-round-trip.py
@@ -1,58 +1,86 @@
-#통과 실패..
-import heapq
 import sys
 
 INT_MAX = sys.maxsize
 
-
+# 변수 선언 및 입력:
 n, m, x = tuple(map(int, input().split()))
 
-graph = [[] for _ in range(n + 1)]
-graph_reversed = [[] for _ in range(n + 1)]
-edges = [tuple(map(int, input().split())) for _ in range(m)]
+# 간선을 입력받습니다.
+edges = [
+    tuple(map(int, input().split()))
+    for _ in range(m)
+]
 
-for i in range(m):
-    X, y, z = edges[i]
-    graph[X].append((y, z))
-    graph_reversed[y].append((X, z))
+graph1 = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
+graph2 = [
+    [0] * (n + 1)
+    for _ in range(n + 1)
+]
 
-
-def Dijkstra(k, graph):#시작점
-    pq = []
-    dist = [INT_MAX] * (n + 1)
-
-    dist[k] = 0
-
-    # (거리, 정점 번호) 형태로 넣어줘야 합니다.
-    heapq.heappush(pq, (0, k)) # 우선순위 큐에 시작점/ 거리에 가까운 곳이 먼저 나와야
-
-    while pq:
-        min_dist, min_index = heapq.heappop(pq)
-
-        if min_dist != dist[min_index]:
-            continue
+# 그래프에 있는 모든 노드들에 대해
+# 초기값을 전부 아주 큰 값으로 설정
+dist1 = [INT_MAX] * (n + 1)
+dist2 = [INT_MAX] * (n + 1)
+ans = 0
 
 
-        for target_index, target_dist in graph[min_index]:
-            new_dist = dist[min_index] + target_dist
-            if dist[target_index] >= new_dist:
-                dist[target_index] = new_dist
-                heapq.heappush(pq, (new_dist, target_index))
+def dijkstra(dist, graph):
+    # visited값을 전부 False로 초기화 해놓고 시작합니다.
+    visited = [False] * (n + 1)
+
+    # 시작위치에는 dist값을 0으로 설정
+    dist[x] = 0
+
+    # O(|V|^2) 다익스트라 코드
+    for i in range(1, n + 1):
+        # V개의 정점 중 
+        # 아직 방문하지 않은 정점 중
+        # dist값이 가장 작은 정점을 찾아줍니다.
+        min_index = -1
+        for j in range(1, n + 1):
+            if visited[j]:
+                continue
+            
+            if min_index == -1 or dist[min_index] > dist[j]:
+                min_index = j
+
+        # 최솟값에 해당하는 정점에 방문 표시를 진행합니다.
+        visited[min_index] = True
+
+        # 최솟값에 해당하는 정점에 연결된 간선들을 보며
+        # 시작점으로부터의 최단거리 값을 갱신해줍니다.
+        for j in range(1, n + 1):
+            # 간선이 존재하지 않는 경우에는 넘어갑니다.
+            if graph[min_index][j] == 0:
+                continue
+
+            dist[j] = min(dist[j], dist[min_index] + graph[min_index][j])
 
 
-    return dist
+# 그래프를 인접행렬로 표현
+for a, b, c in edges:
+    graph1[a][b] = c
 
+# 1차 다익스트라 진행 (x번 정점에서 i번 정점으로 가는 최단거리)
+dijkstra(dist1, graph1)
 
-dist_a = Dijkstra(x, graph)
-dist_b = Dijkstra(x, graph_reversed)
+# 주어진 그래프에서
+# 간선을 전부 뒤집어준 뒤
+# 다시 다익스트라를 진행합니다.
+# 이렇게 되면
+# 마치 i번 정점에서 x번 정점으로 가는 각각의 최단거리가 구해지는
+# 효과를 얻을 수 있게 됩니다.
+for a, b, c in edges:
+    graph2[b][a] = c
 
+dijkstra(dist2, graph2)
 
+# 각 정점에 대해 왕복 거리를 계산한 뒤
+# 이 중 최댓값을 계산합니다.
+for i in range(1, n + 1):
+    ans = max(ans, dist1[i] + dist2[i])
 
-result = 0
-
-
-for i in range(1, n+1):
-    result = max(result, dist_a[i] + dist_b[i])
-
-
-print(result)
+print(ans)
