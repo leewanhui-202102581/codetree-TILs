f2b297c7662c39e57972c583991354aea05b8b1c
Wanhui_Lee <100890687+leewanhui-202102581@users.noreply.github.com>
2023-11-29 16:13:42 +0900

    Title: 가장 긴 왕복 거리, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231129/가장 긴 왕복 거리/longest-round-trip.py
@@ -0,0 +1,57 @@
+import heapq
+import sys
+
+INT_MAX = sys.maxsize
+
+
+n, m, x = tuple(map(int, input().split()))
+
+graph = [[] for _ in range(n + 1)]
+graph_reversed = [[] for _ in range(n + 1)]
+edges = [tuple(map(int, input().split())) for _ in range(m)]
+
+for i in range(m):
+    x, y, z = edges[i]
+    graph[x].append((y, z))
+    graph_reversed[y].append((x, z))
+
+
+def Dijkstra(k, graph):#시작점
+    pq = []
+    dist = [INT_MAX] * (n + 1)
+
+    dist[k] = 0
+
+    # (거리, 정점 번호) 형태로 넣어줘야 합니다.
+    heapq.heappush(pq, (0, k)) # 우선순위 큐에 시작점/ 거리에 가까운 곳이 먼저 나와야
+
+    while pq:
+        min_dist, min_index = heapq.heappop(pq)
+
+        if min_dist != dist[min_index]:
+            continue
+
+
+        for target_index, target_dist in graph[min_index]:
+            new_dist = dist[min_index] + target_dist
+            if dist[target_index] > new_dist:
+                dist[target_index] = new_dist
+                heapq.heappush(pq, (new_dist, target_index))
+
+
+    return dist
+
+
+dist_a = Dijkstra(x, graph)
+dist_b = Dijkstra(x, graph_reversed)
+
+
+
+result = 0
+
+
+for i in range(1, n+1):
+    result = max(result, dist_a[i] + dist_b[i])
+
+
+print(result)
